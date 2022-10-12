3eb9a4a1387bf545397db1e425c8d7ecfbee57a9
Arash_M <moghani.arash@gmail.com>
2022-10-12 16:04:11 +0330

    init

---
+++ add_iran_routes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#set -x
+iran_interface="x.x.x.x"
+input="iran_ips.txt"
+getopts 'arh' OPTION;
+#echo $OPTION
+case "$OPTION" in
+  a)
+    while IFS= read -r line
+    do
+      #echo "add $line"
+      route add -net $line gw $iran_interface metric 50
+    done < "$input"
+    ;;
+  r)
+    while IFS= read -r line
+    do
+      #echo "del $line"
+      route del -net $line
+    done < "$input"
+    ;;
+  h|*)
+    echo "Use -a to add and -r to remove routes. -h for help"
+    ;;
+esac
